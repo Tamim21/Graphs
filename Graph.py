fff2867011a79f07a59495a9cd2e5a72bba3c98c
Omaressam2002 <omarnewessam1@gmail.com>
2023-05-20 01:08:02 +0300

    directed we undirected.

--- Graph.py
+++ Graph.py
@@ -3,17 +3,23 @@ class Graph:  # adj list
     def __init__(self, vs, es):
         self.vs = vs  # set of vertices {v,v,...}
         self.es = es  # dict of edges {(u,v) : weight}
-        self.ass_neighbors()
-    def ass_neighbors(self): # |E|
-        for rec in self.es:
-            rec[0].neighbors.add(rec[1])
-            rec[1].neighbors.add(rec[0])
+    def ass_neighbors(self,Dijkstra = False): # |E|
+        for v in self.vs:
+            v.clearNeighbors()
+        if Dijkstra: # directed
+            for rec in self.es:
+                rec[0].neighbors.add(rec[1])
+        else : # undirected
+            for rec in self.es:
+                rec[0].neighbors.add(rec[1])
+                rec[1].neighbors.add(rec[0])
     def add(self,edge):
         self.es.add(edge)
         self.vs.add(edge.key[0])
         self.vs.add(edge.key[1])
         self.ass_neighbors()
     def prim(self, root): # V*getmin + E*update,,,,, V*lgv + E*lgv
+        self.ass_neighbors()
         s = {node: [float("inf"), root] for node in self.vs}  # dict = node : (dist,parent)
         s[root] = [0, root]  # root: (dist = 0, parent = root)
         heap = Heap(s)  # vlgv
@@ -41,6 +47,7 @@ class Graph:  # adj list
             print(f"{i[0].name} --- {i[1].name} ------------- {s[i]}")
 
     def Dijkstra(self, root):
+        self.ass_neighbors(True)
         for e in self.es: # checking for negative edges
             if self.es[e] < 0:
                 print("Dijkstra doesnt work with -ve weighted edges, try another algorithm!!!")
@@ -60,11 +67,6 @@ class Graph:  # adj list
                         heap.array[v.posInHeap][1] = [self.es[(u[0], v)] + dist,u[0]]  # weight and parent update in the heap
                         v.parent = u[0]
                         heap.up_heapify(v.posInHeap) # update el heap
-                elif (v, u[0]) in self.es and (v.posInHeap != -1) :
-                    if heap.array[v.posInHeap][1][0] > self.es[(v,u[0])] + dist:
-                        heap.array[v.posInHeap][1] = [self.es[(v,u[0])] + dist,u[0]]
-                        v.parent = u[0]
-                        heap.up_heapify(v.posInHeap) # update el heap after minimizing weight
         self.printSP(s,root)
 
     def printSP(self,s,source):
--- main.py
+++ main.py
@@ -10,16 +10,14 @@ six = vertex(6)
 seven = vertex(7)
 eight = vertex(8)
 vertices = {z,o,t,th,f,fi,six}
-edges = {(z,o):2,(z,th):8,(o,th):4,(o,f):1,(o,t):10,(o,fi):2,(t,fi):5,(th,f):5,(th,six):1,(f,six):4,(f,fi):2,(fi,six):7}
+edges = {(z,o):3,(z,th):8,(o,th):4,(o,f):1,(o,t):10,(o,fi):2,(t,fi):5,(th,f):5,(th,six):1,(f,six):4,(f,fi):2,(fi,six):7}
 g = Graph(vertices,edges)
-g.prim(z)
 g.Dijkstra(z)
 
 
 
-v = {z,o,t,th,f,fi,six,seven,eight}
-e ={(z,o):4,(z,seven):8,(o,t):8,(o,seven):11,(t,th):7,(t,eight):2,(t,fi):4,(th,f):9,(th,fi):14,(f,fi):10,(fi,six):2,(six,seven):1,(six,eight):6,(seven,eight):7}
+v = {z,o,t,th,f,fi,six}
+e ={(z,o):2,(z,th):1,(o,th):3,(o,f):10,(t,z):4,(t,fi):5,(th,t):2,(th,f):7,(th,fi):8,(th,six):4,(f,six):6,(six,fi):1}
 g = Graph(v,e)
 g.prim(z)
-g.Dijkstra(z)
 
